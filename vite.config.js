cabb5cd46b43802ed39f2c152241e9319bff0722
tmanyanov <2737310+darkwebdev@users.noreply.github.com>
2025-06-16 14:08:52 +0200

    Fix routing

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr';
+import svgr from 'vite-plugin-svgr'
 
-export default defineConfig({
-    plugins: [
-        react(),
-        svgr(),
-    ],
-    test: {
-        /* for example, use global to avoid globals imports (describe, test, expect): */
-        globals: true,
-    },
-    base: '/ric/'
+
+export default defineConfig(async () => {
+    const { viteStaticCopy } = await import('vite-plugin-static-copy');
+    return {
+        plugins: [
+            react(),
+            svgr(),
+            viteStaticCopy({
+                targets: [
+                    {
+                        src: '404.html',
+                        dest: '.'
+                    }
+                ]
+            }),
+        ],
+        test: {
+            /* for example, use global to avoid globals imports (describe, test, expect): */
+            globals: true,
+        },
+        base: '/ric/'
+    };
 })
